0aef369998bfe31f8956fa75c6042c01a9253619
AnatolyKladov <tboundless9@gmail.com>
2022-12-06 16:33:50 +0700

    Приветствие

---
+++ lab7.js
@@ -0,0 +1,20 @@
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    let userSex = document.getElementById('sex').value;
+
+    let greeting;
+    if(userAge <= 17) {
+        greeting = 'Здравствуй, ';
+    }
+    else {
+        greeting = 'Здравствуйте, ';
+    }
+    alert(greeting + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userAge)
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+    }
