442cc2dc06bf79c586856d447d4c751774d9900d
Анатолий Кладов <tboundless9@gmail.com>
2022-12-28 19:15:38 +0700

    Время

---
+++ lab8.js
@@ -0,0 +1,25 @@
+function showDate(params) {
+    let out = document.getElementById('today');
+    let today = new Date();
+    out.innerHTML = 'Сегодня: ' + today.toLocaleDateString ('ru-RU');
+}
+function showDaysCount(params){
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.ceil (daysCount);
+    result.innerHTML = 'С даты рождения прошло дней:' + daysCount;
+}
+
+window.addEventListener('load', showDate);
+
+function showTime() {
+    let outTime = document.getElementById('time');
+    let currentTime = new Date();
+    outTime.innerHTML = currentTime.toLocaleTimeString('ru');
+}
+
+window.addEventListener('load', showTime);
+setInterval(showTime, 1000);
